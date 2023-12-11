bd4513001fcca8b5df4e1e599583cf0b21b172cf
madeleine plumridge <madeleine.plumridge@gmail.com>
2023-12-11 21:31:56 +0100

    dark mode button

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,9 +1,13 @@
 import { Application } from "@hotwired/stimulus"
+import DarkmodeController from "controllers/darkmode_controller";
+
 
 const application = Application.start()
+application.register("darkmode", DarkmodeController);
 
 // Configure Stimulus development experience
 application.debug = false
 window.Stimulus   = application
+
 
 export { application }
